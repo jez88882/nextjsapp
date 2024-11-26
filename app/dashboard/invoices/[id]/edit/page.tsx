a6a2e9214d3763e23259081381a07672d0fe322b
jiaen zhang <jiaen.zhang98@gmail.com>
2024-11-26 12:32:25 +0800

    set up invoice forms

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,17 @@
+import Form from "@/app/ui/invoices/edit-form"
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data"
+import { notFound } from "next/navigation"
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params
+  const id = params.id
+  const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+  if (!invoice) {
+    notFound()
+  }
+  return (
+    <main>
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
